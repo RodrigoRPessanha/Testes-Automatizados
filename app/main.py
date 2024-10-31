d638ca65bbdf6e49d3454dd8168e9a4061ceef1b
Thiago <thiagofagundesdossantos@gmail.com>
2024-10-30 23:34:12 -0300

    Backend adjust

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.controllers import doctor_controller, patient_controller, appointment_controller
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Substitua pelo domínio do seu frontend, se necessário
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos os métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
+)
 
 app.include_router(doctor_controller.router)
 app.include_router(patient_controller.router)
--- app/repositories/patient_repository.py
+++ app/repositories/patient_repository.py
@@ -1,41 +1,44 @@
 import sqlite3
+from typing import List
 from app.entities.patient import Patient
 
 class PatientRepository:
     def __init__(self, db_path: str = "database.db"):
-        self.connection = sqlite3.connect(db_path)
+        self.db_path = db_path
         self.create_table()
 
+    def create_connection(self):
+        return sqlite3.connect(self.db_path)
+
     def create_table(self):
-        with self.connection:
-            self.connection.execute("""
+        with self.create_connection() as connection:
+            connection.execute("""
                 CREATE TABLE IF NOT EXISTS patients (
-                    id INTEGER PRIMARY KEY,
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                     name TEXT NOT NULL,
                     age INTEGER NOT NULL
                 )
             """)
 
     def add_patient(self, patient: Patient):
-        with self.connection:
-            self.connection.execute(
-                "INSERT INTO patients (id, name, age) VALUES (?, ?, ?)",
-                (patient.id, patient.name, patient.age)
+        with self.create_connection() as connection:
+            connection.execute(
+                "INSERT INTO patients (name, age) VALUES (?, ?)",
+                (patient.name, patient.age)
             )
 
-    def get_all_patients(self):
-        cursor = self.connection.cursor()
-        cursor.execute("SELECT id, name, age FROM patients")
-        rows = cursor.fetchall()
-        return [Patient(id=row[0], name=row[1], age=row[2]) for row in rows]
+    def get_all_patients(self) -> List[Patient]:
+        with self.create_connection() as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT id, name, age FROM patients")
+            rows = cursor.fetchall()
+            return [Patient(id=row[0], name=row[1], age=row[2]) for row in rows]
 
     def get_patient_by_id(self, patient_id: int) -> Patient:
-        cursor = self.connection.cursor()
-        cursor.execute("SELECT id, name, age FROM patients WHERE id = ?", (patient_id,))
-        row = cursor.fetchone()
-        if row:
-            return Patient(id=row[0], name=row[1], age=row[2])
-        return None
-
-    def close(self):
-        self.connection.close()
+        with self.create_connection() as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT id, name, age FROM patients WHERE id = ?", (patient_id,))
+            row = cursor.fetchone()
+            if row:
+                return Patient(id=row[0], name=row[1], age=row[2])
+            return None
